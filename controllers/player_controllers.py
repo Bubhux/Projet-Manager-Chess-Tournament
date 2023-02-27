9002ec5b1331403bf5646f75ac825e5dabfd4ec5
Bubhux <bubhuxpaindepice@gmail.com>
2023-02-27 14:14:10 +0100

    Modification sur la partie models

--- controllers/player_controllers.py
+++ controllers/player_controllers.py
@@ -1,34 +1,42 @@
+"""Module player_controllers."""
 from views.player_menu import CreatePlayer
 from models.player_models import Player
-#from controllers.database_controllers import DataBase, save_database, update_player_rank
+from controllers.database_controllers import DataBase
+
 
 def create_player():
+    """Fonction création de joueurs renvoie un joueur."""
+    database = DataBase()
 
-    # Récupération des données du joueur
+    # Récupération des données du joueur.
     user_data = CreatePlayer().display_create_player_menu()
 
-    # Création du joueur
+    # Création du joueur.
     player = Player(
-        user_data["name"],
-        user_data["surname"],
-        user_data["birthday_date"],
-        user_data["sexe"],
-        user_data["total_score"],
-        user_data["tournament_score"],
-        user_data["rank"])
+        user_data['name'],
+        user_data['surname'],
+        user_data['birthday_date'],
+        user_data['sexe'],
+        user_data['rank'],
+        user_data['total_score'])
 
     serialized_player = player.serialized_player()
     print(serialized_player)
 
-    #save_database("players", serialized_player)
+    database.save_database("players", serialized_player)
 
     return player
 
+
 def update_rankings(player, rank, score=True):
+    """Fonction de mise a jour du classement."""
+    database = DataBase()
+
     if score:
         player.total_score += player.tournament_score
+
     player.rank = rank
     serialized_player = player.serialized_player(save_tournament_score=True)
-    print(serialized_player['name'])
-    update_player_rank("players", serialized_player)
-    print(f"Mise à jour du rang de {player}:\nScore total: {player.total_score}\nRang: {player.rank}")
+    database.update_player_rank("players", serialized_player)
+    print(f"Mise à jour du rang de {player}:\nScore total: {player.total_score} pts\nRang: {player.rank}")
+    print("==================================================================")
--- controllers/tournament_controllers.py
+++ controllers/tournament_controllers.py
@@ -1,19 +1,21 @@
+"""Module tournament_controllers."""
 from models.tournament_models import Tournament
-from views.tournament_menu import CreateTournament, LoadingTournament
 from views.view_user_entry import ViewUserEntry
+from views.tournament_menu import CreateTournament, LoadingTournament
+from views.player_menu import LoadindPlayer
 from controllers.player_controllers import create_player, update_rankings
-#from controllers.database_controllers import DataBase, save_database, update_database, loading_player, loading_tournament
-#from models.tour_models import create_tour
+from controllers.database_controllers import DataBase
 
 
 def create_tournament():
-
+    """Fonction création de tournoi."""
     menu = ViewUserEntry()
+    database = DataBase()
 
-    # Récupération des données du tournoi
+    # Récupération des données du tournoi.
     user_data = CreateTournament().display_tournament_menu()
 
-    # Choix chargement des joueurs
+    # Choix chargement des joueurs.
     user_input = menu.user_entry(
         message_display="Sélectionner\n1 - Créer des joueurs\n2 - Charger des joueurs\n> ",
         message_error="Entrer un choix valide",
@@ -21,7 +23,7 @@ def create_tournament():
         assertions=["1", "2"]
     )
 
-    # Chargement des joueurs
+    # Chargement des joueurs.
     if user_input == "2":
         players = []
         user_input = menu.user_entry(
@@ -29,58 +31,64 @@ def create_tournament():
             message_error="Entrer 0 ou 1",
             value_type="numeric"
         )
+        serialized_players = LoadindPlayer().display_loading_player(
+            number_players_loading=user_input
+        )
+        for serialized_player in serialized_players:
+            player = database.loading_player(serialized_player)
+            players.append(player)
 
-    # Création des joueurs
+    # Création des joueurs.
     else:
         print(f"Création de {str(user_data['number_players'])} joueurs")
         players = []
         while len(players) < user_data['number_players']:
             players.append(create_player())
 
-    # Création du tournoi
+    # Création du tournoi.
     tournament = Tournament(
-        user_data["name"],
-        user_data["location"],
-        user_data["date"],
-        user_data["time_control"],
+        user_data['name'],
+        user_data['location'],
+        user_data['date'],
+        user_data['time_control'],
         players,
-        user_data["description"],
-        user_data["number_tours"])
-    
-    #save_database("tournaments", tournament.serialized_tournament())
+        user_data['description'],
+        user_data['number_tours'])
+    database.save_database("tournaments", tournament.serialized_tournament())
 
     return tournament
 
-def play_tournament(tournament, loading_new_tournament=False):
 
+def play_tournament(tournament, loading_new_tournament=False):
+    """Affiche le début du tournoi."""
     menu = ViewUserEntry()
+    database = DataBase()
 
     print(f"Début du tournoi {tournament.name}")
     print()
 
     while True:
 
-        # Calcul des tours restant à jouer pour un nouveau tournoi
-        z = 0 
+        # Calcul des tours restant à jouer pour un nouveau tournoi.
+        z = 0
         if loading_new_tournament:
             for tour in tournament.tours:
                 if tour.time_end == "":
-                   z += 1
+                    z += 1
             number_tours_play = tournament.number_tours - z
             loading_new_tournament = False
         else:
             number_tours_play = tournament.number_tours
 
         for i in range(number_tours_play):
-                
-            # Création du tour
+            # Création du tour.
             tournament.create_tour(tour_number=i+z)
 
-            # Joue le dernier tour créé
+            # Joue le dernier tour créé.
             current_tour = tournament.tours[-1]
             print(current_tour.time_start + " : Début du " + current_tour.name)
 
-            # Le tour terminé on passe au tour suivant
+            # Le tour terminé on passe au tour suivant.
             while True:
                 user_input = menu.user_entry(
                     message_display="Sélectionner\n"
@@ -95,58 +103,57 @@ def play_tournament(tournament, loading_new_tournament=False):
                 )
                 print()
 
-                # Tour suivant 
+                # Tour suivant.
                 if user_input == "1":
                     current_tour.match_completed()
                     break
 
-                # Affiche les classements 
+                # Affiche les classements.
                 elif user_input == "2":
                     print(f"Classement du tournoi {tournament.name}\n")
                     for i, player in enumerate(tournament.tournament_rankings()):
-                        print(f"{str(i + 1)} - {player}")
+                        print(f"{str(i+1)} - Joueur {player}")
+                    print()
 
-                # Changement des rangs
+                # Changement des rangs.
                 elif user_input == "3":
                     for player in tournament.players:
-                        rank = menu.get_user_entry(
-                            message_display=f"Classement de {player}:\n> ",
+                        rank = menu.user_entry(
+                            message_display=f"Classement du joueur {player}:\nSaisir le nouveau rang > ",
                             message_error="Veuillez entrer un nombre entier.",
                             value_type="numeric"
                         )
                         update_rankings(player, rank, score=False)
-                
-                # Sauvegarder le tournoi
+                    print()
+                # Sauvegarder le tournoi.
                 elif user_input == "4":
                     rankings = tournament.tournament_rankings()
                     for i, player in enumerate(rankings):
                         for x_player in tournament.players:
                             if player.name == x_player.name:
-                                x_player.rank = str(i + 1)
-                    #update_database("tournaments", tournament.serialized_tournament(save_tours=True))
-
-                # Charger un tournoi
+                                x_player.rank = str(i+1)
+                    database.update_tournament_database("tournaments",
+                                                        tournament.serialized_tournament(save_tours=True))
+                # Charger un tournoi.
                 elif user_input == "5":
                     serialized_loading_tournament = LoadingTournament().display_loading_tournament_menu()
-                    tournament = load_tournament(serialized_loading_tournament)
+                    tournament = database.loading_tournament(serialized_loading_tournament)
                     loading_new_tournament = True
                     break
-                
             if loading_new_tournament:
                 break
-
         if loading_new_tournament:
             continue
 
         else:
             break
-    
+
     rankings = tournament.tournament_rankings()
     for i, player in enumerate(rankings):
         for x_player in tournament.players:
             if player.name == x_player.name:
                 x_player.total_score += player.tournament_score
                 x_player.rank = str(i+1)
-    #update_database("tournaments", tournament.serialized_tournament(save_tours=True))
+    database.update_tournament_database("tournaments", tournament.serialized_tournament(save_tours=True))
 
     return rankings
